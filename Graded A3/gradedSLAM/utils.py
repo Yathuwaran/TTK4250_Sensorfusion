757488e5e95469221718df6d49ea1819df15e848
Håkon Haugann <Haugann@dhcp-10-24-74-244.wlan.ntnu.no>
2020-11-03 10:43:00 +0100

    Added p2c and c2p

--- Graded A3/gradedSLAM/utils.py
+++ Graded A3/gradedSLAM/utils.py
@@ -6,3 +6,13 @@ def wrapToPi(angle):
 def rotmat2d(angle):
     return np.array([[np.cos(angle), -np.sin(angle)],
                      [np.sin(angle),  np.cos(angle)]])
+
+def c2p(x, y):
+    rho = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    return(rho, phi)
+
+def p2c(rho, phi):
+    x = rho * np.cos(phi)
+    y = rho * np.sin(phi)
+    return(x, y)
